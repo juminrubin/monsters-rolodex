4d8dbc7fe1d22c669401cbacbf07dd7eda864c7b
Jumin Rubin <juminrubin@gmail.com>
2019-12-15 20:06:31 +0100

    Updates

---
+++ precache-manifest.c12d659abc67db06d5dbab145cccba29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9fc1fca11d360b5d6f11aa3c5a1b3b8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "128da3c2c71158e28d70",
+    "url": "/monsters-rolodex/static/css/main.c294cff7.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "128da3c2c71158e28d70",
+    "url": "/monsters-rolodex/static/js/main.8f3923ea.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
